97dda936107778ebfff3b8f5b58dbd26d50d5164
drashti04 <85118452+drashti-thekdi@users.noreply.github.com>
2026-05-13 19:38:26 +0530

    Initial commit

---
+++ src/data/tracks.js
@@ -0,0 +1,104 @@
+const TRACKS = [
+
+	{
+		title: 'Karam Apnaa Apnaa Song',
+		artist: 'Rockot',
+		cover: 'https://thumbs.dreamstime.com/b/glowing-treble-clef-music-notes-abstract-colorful-energy-emit-vibrant-light-against-dark-backdrop-great-themed-391042538.jpg',
+		src: './songs/Karam Apnaa Apnaa Title Song [tvshowsongs].mp3',
+	},
+
+	{
+		title: 'Tere Liye Song',
+		artist: 'Rockot',
+		cover: 'https://plus.unsplash.com/premium_vector-1727391181054-e93a303b92cd?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bXVzaWN8ZW58MHx8MHx8fDA%3D',
+		src: './songs/chat-of-world_tere-liye-kailash-kher-star-plus.mp3',
+	},
+
+	{
+		title: 'FalguniPathak Song',
+		artist: 'Rockot',
+		cover: 'https://media.istockphoto.com/id/1319479588/photo/the-musicians-were-playing-rock-music-on-stage-there-was-an-audience-full-of-people-watching.jpg?s=612x612&w=0&k=20&c=OrGoVzFYygF904aMkM38N_v53yyBI5D_aWkpZZhTvEY=',
+		src: './songs/Chudi Dandiya Queen Hits 128 Kbps.mp3',
+	},
+
+	{
+		title: 'Diya aur Baati Hum',
+		artist: 'Rockot',
+		cover: 'https://static.vecteezy.com/system/resources/thumbnails/069/792/011/small/glowing-musical-notes-above-sheet-music-night-bokeh-background-music-website-photo.jpg',
+		src: './songs/Diya-Aur-Baati-Hum.mp3',
+	},
+
+	{
+		title: 'FalguniPathak Song',
+		artist: 'Rockot',
+		cover: 'https://thumbs.dreamstime.com/b/music-illustration-25677286.jpg',
+		src: './songs/Meri Chunar Udd Udd Jaye Dandiya Queen Hits 128 Kbps.mp3',
+	},
+
+
+	{
+		title: 'Navya Serial',
+		artist: 'Rockot',
+		cover: 'https://png.pngtree.com/thumb_back/fh260/background/20250408/pngtree-musical-illustration-featuring-colorful-swirling-waves-of-music-notes-image_17178153.jpg',
+		src: './songs/Navya-Serial-Song-.mp3',
+	},
+
+
+	{
+		title: 'Sath Nibhana Sathiya',
+		artist: 'Rockot',
+		cover: 'https://cdn.pixabay.com/audio/2025/05/29/12-35-32-759_200x200.jpg',
+		src: './songs/Saath-Nibhaana-Saathiya.mp3',
+	},
+
+
+	{
+		title: 'FalguniPathak Song',
+		artist: 'Rockot',
+		cover: 'https://plus.unsplash.com/premium_photo-1682125232467-8fcf48869840?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bXVzaWMlMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D',
+		src: './songs/Saawan Mein Dandiya Queen Hits 128 Kbps.mp3',
+	},
+
+
+	{
+		title: 'Serial Song',
+		artist: 'Rockot',
+		cover: 'https://png.pngtree.com/thumb_back/fh260/background/20221224/pngtree-blue-musical-notes-background-image_1530362.jpg',
+		src: './songs/Saraswatichandra.mp3',
+	},
+
+	{
+		title: 'Tumhari Pakhi Song',
+		artist: 'Rockot',
+		cover: 'https://img.freepik.com/premium-vector/colorful-musical-note-art-with-vibrant-splashes-musical-note-black-surrounded_53876-651215.jpg?semt=ais_hybrid&w=740&q=80',
+		src: './songs/shakti-rayzon_bol-na-dil-se-tumhari-pakhi.mp3',
+	},
+
+
+	{
+		title: 'FalguniPathak Song',
+		artist: 'Rockot',
+		cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY1iHEtGpPZPkn7NyTN-MvFW936EUuBZ-vVA&s',
+		src: './songs/Yaad Piya Ki Aane Lagi Pyaar Koi Khel Nahin 128 Kbps.mp3',
+	},
+
+	{
+		title: 'FalguniPathak Song',
+		artist: 'Rockot',
+		cover: 'https://www.shutterstock.com/image-photo/yellow-neon-musical-notes-gentle-260nw-2705787329.jpg',
+		src: './songs/Yeh Kisne Jadu Kiya Dandiya Queen Hits 128 Kbps.mp3',
+	},
+
+
+	{
+		title: 'Serial Song',
+		artist: 'Rockot',
+		cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9piTQ7pvolU-nxqb6ZJcBq151BJzLn6LE6Q&s',
+		src: './songs/ZIndagi Mein Aaj Lamhe Khaas Aa Gaye.....mp3',
+	},
+	
+	
+	
+];
+
+export default TRACKS;
